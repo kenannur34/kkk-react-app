4337fe214de354bbec770d9b11f966577ffa5218
Kenan Nur <kenan.nur@botgate.ai>
2022-05-11 14:38:37 +0300

    iframe test

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <iframe title='cjTest' src="https://oauth.sandbox.botgate.ai" width="500" height="500"></iframe>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
       </header>
     </div>
   );
